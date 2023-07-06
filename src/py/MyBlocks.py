ae52c64803e3314d7f3c0cc739b5a4443e80e483
chengchanghui <3140102120@zju.edu.cn>
2023-07-06 21:04:13 +0800

    feat 更新ui界面

--- src/py/MyBlocks.py
+++ src/py/MyBlocks.py
@@ -1,14 +1,55 @@
 from ..ui.Ui_AccountBlock import Ui_AccountBlock as AccountBlock
+from ..ui.Ui_MainWindow import Ui_AccountList
 from ..ui.Ui_ConfirmationBlock import Ui_ConfirmationBlock as ConfirmBlock
+from ..ui.Ui_ConfirmationList import Ui_ConfirmationList as ConfirmList
 from ..py.MySteamClient import MySteamClient, MyConfirmation
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import requests, time
+import requests, time, json
 
 
-class MyConfirmationsWindow(QWidget):
-    pass
+# conf设置成多个steam_account的数组
+class MyMainWindow(QMainWindow, Ui_AccountList):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.SteamClientList = []
+        self._inner_widget = None
+        self._inner_widget_layout = None
+        # 读取steam信息
+        steam_conf_list = json.load(open("./conf/test.json", "r", encoding="utf8"))
+        for i in range(0, len(steam_conf_list)):
+            # self.SteamClientList.append(MySteamClient(steam_conf_list[i]))
+            self.add_steam_client(steam_conf_list[i])
+            time.sleep(5)
+            pass
+
+    def add_steam_client(self, steam_conf):
+        if not self._inner_widget:
+            self._inner_widget = QWidget()
+            self._inner_widget_layout = QVBoxLayout()
+            self._inner_widget.setLayout(self._inner_widget_layout)
+        ui_steam_account = MyAccountBlock(steam_conf)
+        self._inner_widget_layout.addWidget(ui_steam_account)
+        self.accountList.setWidget(self._inner_widget)
+        self.accountList.show()
+
+
+class MyConfirmationList(QScrollArea, ConfirmList):
+    def __init__(self, parent=None):
+        super(MyConfirmationList, self).__init__(parent)
+        self.setupUi(self)
+        self._inner_widget = QWidget()
+        self._inner_widget_layout = QVBoxLayout()
+        self._inner_widget.setLayout(self._inner_widget_layout)
+
+    def showConfirmations(self, confirmatios):
+        self._confirmations = confirmatios
+        for confirmation in confirmatios:
+            self._inner_widget_layout.addWidget(confirmation)
+        self.setWidget(self._inner_widget)
+        self.show()
 
 
 class MyConfirmBlock(QWidget, ConfirmBlock):
@@ -39,32 +80,33 @@ class MyConfirmBlock(QWidget, ConfirmBlock):
             try:
                 rsp = self.__accept(self.confirmation.get_trade_offer())
                 if rsp.get("success", False):
-                    self.show_res(True)
+                    self.show_res(0)
                 else:
-                    self.show_res(False)
+                    self.show_res(2)
             except:
-                self.show_res(False)
+                self.show_res(2)
         pass
 
     def reject(self):
         if self.__reject:
             try:
                 rsp = self.__reject(self.confirmation.get_trade_offer())
-                if rsp.get("success", False):
-                    self.show_res(True)
+                print(rsp)
+                if rsp.get("tradeofferid", False):
+                    self.show_res(1)
                 else:
-                    self.show_res(False)
+                    self.show_res(3)
             except:
-                self.show_res(False)
+                self.show_res(3)
         pass
 
+    # res: 0-确认成功, 1-取消成功, 2-确认失败, 3-取消失败
     def show_res(self, res):
+        res_str_list = ["确认成功", "取消成功", "确认失败", "取消失败"]
         self.confirm.deleteLater()
         self.cancel.deleteLater()
         label = QLabel()
-        res_str = str("确认失败")
-        if res:
-            res_str = str("确认成功")
+        res_str = res_str_list[res]
         label.setText(res_str)
         layout = QVBoxLayout()
         layout.addWidget(label)
@@ -116,8 +158,10 @@ class MyAccountBlock(QWidget, AccountBlock):
 
         self.account.setAttribute(Qt.WA_TranslucentBackground)
         self.account.setWindowFlags(Qt.FramelessWindowHint)
+        self.account.setAlignment(Qt.AlignCenter)
         self.twoFactorCode.setAttribute(Qt.WA_TranslucentBackground)
         self.twoFactorCode.setWindowFlags(Qt.FramelessWindowHint)
+        self.twoFactorCode.setAlignment(Qt.AlignCenter)
 
     def showCode(self, code):
         self.twoFactorCode.setText(code)
@@ -125,25 +169,20 @@ class MyAccountBlock(QWidget, AccountBlock):
 
     def showConfirmations(self):
         # confirmations = self.__steam_client.get_confirmations()
-        self.confirmationsWindow = MyConfirmationsWindow()
-        self.confirmationsWindow.resize(400, 500)
-        self.confirmationsWindow.setWindowModality(Qt.WindowModality.ApplicationModal)
-        self.confirmationsWindow.show()
+        self.confirmationList = MyConfirmationList()
+        self.confirmationList.setWindowModality(Qt.WindowModality.ApplicationModal)
 
         confirmations = self.__steam_client.get_confirmations()
+        confirmaionBlocks = []
         for confirmation in confirmations:
-            myConfirmaBlock = MyConfirmBlock(confirmation)
-
-            if not self.confirmationsWindow.layout():
-                layout = QVBoxLayout()
-                self.confirmationsWindow.setLayout(layout)
-            myConfirmaBlock.show()
-            self.confirmationsWindow.layout().addWidget(myConfirmaBlock)
-            myConfirmaBlock._set_accept(
+            myConfirmBlock = MyConfirmBlock(confirmation)
+            myConfirmBlock._set_accept(
                 lambda trade_offer: self.__steam_client._confirm_transaction(
                     trade_offer
                 )
             )
-            myConfirmaBlock._set_reject(
+            myConfirmBlock._set_reject(
                 lambda trade_offer: self.__steam_client.cancel_trade_offer(trade_offer)
             )
+            confirmaionBlocks.append(myConfirmBlock)
+        self.confirmationList.showConfirmations(confirmaionBlocks)
